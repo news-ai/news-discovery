53b37b29dfeec5bf4e7f90ba2e70ebbc9b18f844
Julie Pan <ycp217@nyu.edu>
2016-02-21 18:09:16 -0500

    add more links + post_publisher

--- context.py
+++ context.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 
+base_url = 'https://context.newsai.org/api'
 
 def get_login_token():
     headers = {
@@ -12,7 +13,7 @@ def get_login_token():
         "username": os.environ.get("NEWSAI_CONTEXT_API_USERNAME"),
         "password": os.environ.get("NEWSAI_CONTEXT_API_PASSWORD"),
     }
-    r = requests.post("https://context.newsai.org/api/jwt-token/",
+    r = requests.post(base_url + "/jwt-token/",
                       headers=headers, data=json.dumps(payload), verify=False)
     data = json.loads(r.text)
     token = data.get('token')
@@ -20,6 +21,9 @@ def get_login_token():
 
 
 def post_article(url, token):
+    if token is None:
+        print 'Missing token'
+        return
     headers = {
         "content-type": "application/json",
         "accept": "application/json",
@@ -30,6 +34,27 @@ def post_article(url, token):
         "url": url,
     }
 
-    r = requests.post('https://context.newsai.org/api/articles/',
+    r = requests.post(base_url + '/articles/',
                       headers=headers, data=json.dumps(payload), verify=False)
-    return r.text
+    return r
+
+
+def post_publisher(url, publisher, name, token):
+    if token is None:
+        print 'Missing token'
+        return
+    headers = {
+        "content-type": "application/json",
+        "accept": "application/json",
+        "authorization": "Bearer " + token
+    }
+
+    payload = {
+        "url": url,
+        'publisher': publisher,
+        'name': name
+    }
+
+    r = requests.post(base_url + '/publishers/',
+                      headers=headers, data=json.dumps(payload), verify=False)
+    return r
--- feed.py
+++ feed.py
@@ -12,23 +12,48 @@ def get_rss_links(url, token, tag='id'):
     if len(ret) > 0:
         for link in ret:
             r = context.post_article(link, token)
-            print r
+            if r.status_code == 500:
+                print r.text
+                print link
+
+def test_rss_feed(url, token):
+    d = feedparser.parse(url)
+    for entry in d.get('entries'):
+        print entry
+
+def post_all_feeds():
+    token = context.get_login_token()
+
+    # # when publisher rss feed has custom tags:
+    custom_urls = [
+           ['http://feeds.feedburner.com/TheAtlantic?format=xml', 'feedburner_origlink'],
+           ['http://feeds.foxnews.com/foxnews/latest', 'link'],
+           ['http://espn.go.com/espn/rss/news', 'link'],
+           ['http://espn.go.com/espn/rss/nfl/news', 'link'],
+           ['http://espn.go.com/espn/rss/nba/news', 'link'],
+           ['http://espn.go.com/espn/rss/mlb/news', 'link'],
+           ['http://espn.go.com/espn/rss/nhl/news', 'link'],
+           ['http://espn.go.com/espn/rss/rpm/news', 'link'],
+           ['http://espn.go.com/espn/rss/espnu/news', 'link'],
+           ['http://espn.go.com/espn/rss/ncb/news', 'link'],
+           ['http://espn.go.com/espn/rss/ncf/news', 'link'],
+           ['http://espn.go.com/espn/rss/action/news', 'link'],
+           ['http://espn.go.com/espn/rss/poker/news', 'link'],
+           ['http://www.espnfc.com/rss', 'link']
+           ]
+    for i in xrange(0, len(custom_urls)):
+        get_rss_links(custom_urls[i][0], token, tag=custom_urls[i][1])
+
+    # # # normal case
+    normal_urls = [
+            'http://feeds.reuters.com/reuters/topNews?format=xml',
+            'http://rss.cnn.com/rss/cnn_topstories.rss',
+            'http://feeds.bbci.co.uk/news/rss.xml'
+            ]
+    for url in normal_urls:
+        get_rss_links(url, token)
 
 
-token = context.get_login_token()
-
-# when publisher uses feedburner:
-# feedburner_urls = [
-#        'http://feeds.feedburner.com/TheAtlantic?format=xml'
-#         ]
-# for url in feedburner_urls:
-#     get_rss_links(url, token, tag='feedburner_origlink')
-
-# normal case
-normal_urls = [
-        'http://feeds.reuters.com/reuters/topNews?format=xml',
-        'http://rss.cnn.com/rss/cnn_topstories.rss',
-        'http://feeds.bbci.co.uk/news/rss.xml'
-        ]
-for url in normal_urls:
-    get_rss_links(url, token)
+# url = 'http://espn.go.com/espn/rss/nfl/news'
+# url = 'http://www.espnfc.com/rss'
+# get_rss_links(url, token)
