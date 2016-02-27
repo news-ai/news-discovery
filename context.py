77fe1c54830205accc553d5baf0e710b55159b52
Abhi Agarwal <me@abhiagarwal.com>
2016-02-26 19:27:59 -0500

    Adding some methods

--- context.py
+++ context.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+from urlparse import urlparse
 
 from newspaper import Article
 
@@ -28,18 +29,30 @@ def entity_extraction(keywords, text):
     return []
 
 
-def read_article(url):
+def url_validate(url):
+    url = urlparse(url)
+    return (
+        url.scheme + "://" + url.netloc +
+        url.path, url.scheme + "://" + url.netloc
+    )
+
+
+def read_article(url, token):
     article = Article(url)
     article.download()
     article.parse()
     article.nlp()
 
+    url, publisher = url_validate(url)
+
     data = {}
     data['url'] = url
     data['name'] = article.title  # Get Title
-    data['created_at'] = str(article.publish_date)
+    if article.publish_date:
+        data['created_at'] = str(article.publish_date)
     data['header_image'] = article.top_image
     data['basic_summary'] = article.summary
+    data['authors'] = post_author(publisher, article.authors, token)
 
     # data['keywords'] = article.keywords
     # entity_extraction(article.keywords, article.text)
@@ -56,11 +69,58 @@ def post_article(url, token):
         "authorization": "Bearer " + token
     }
 
-    payload = read_article(url)
+    payload = read_article(url, token)
 
     r = requests.post(base_url + '/articles/',
                       headers=headers, data=json.dumps(payload), verify=False)
     return r
+
+
+def is_author_valid(author_name):
+    if len(author_name.split(' ')) > 3:
+        return False
+    return True
+
+
+def post_author(publisher, authors, token):
+    if token is None:
+        print 'Missing token'
+        return
+
+    if len(authors) is 0:
+        return []
+
+    headers = {
+        "content-type": "application/json",
+        "accept": "application/json",
+        "authorization": "Bearer " + token
+    }
+
+    author_list = []
+
+    for author in authors:
+        r = requests.get(base_url + '/authors/?name=' + author + '&writes_for__url=' + publisher,
+                         headers=headers, verify=False)
+        data = json.loads(r.text)
+        if 'results' in data and len(data['results']) > 0:
+            author_list.append(data['results'][0])
+        else:
+            r = requests.get(base_url + '/publishers/?url=' + publisher,
+                             headers=headers, verify=False)
+            data = json.loads(r.text)
+            if 'results' in data and len(data['results']) is 1:
+                payload = {
+                    'name': author,
+                    'publisher': data['results'][0],
+                }
+                if is_author_valid(author):
+                    r = requests.post(base_url + '/authors/',
+                                      headers=headers, data=json.dumps(payload), verify=False)
+                    data = json.loads(r.text)
+                    print data
+                    author_list.append(data)
+
+    return author_list
 
 
 def post_publisher(url, publisher, name, token):
