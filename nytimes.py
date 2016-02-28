f4d33bec56fa1ea26f93dd33326c410be7db622a
Julie Pan <ycp217@nyu.edu>
2016-02-27 22:25:53 -0500

    freeze requirements

--- nytimes.py
+++ nytimes.py
@@ -3,16 +3,20 @@ import requests
 import json
 import context
 
-newswire = 'http://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=' + \
-    os.environ.get('NYTIMES')
-data = requests.get(newswire).json()
+# if token is not None:
+#     for result in data['results']:
+#         if result.get('url') is not None:
+#             r = context.post_article(result.get('url'), token)
+#             print r
+# else:
+#     print token
 
-token = context.get_login_token()
 
-if token is not None:
-    for result in data['results']:
-        if result.get('url') is not None:
-            r = context.post_article(result.get('url'), token)
-            print r
-else:
-    print token
+def save_nytimes_to_redis():
+    newswire = 'http://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=' + \
+        os.environ.get('NYTIMES')
+    data = requests.get(newswire).json()
+    # token = context.get_login_token()
+    print data
+
+save_nytimes_to_redis()
